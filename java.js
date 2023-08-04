2b7616b357e6a7ceb3682766d17a0e11300d6751
Isidora Stanisavljevska <137770804+moonrose93@users.noreply.github.com>
2023-08-04 16:10:10 +0200

    Update java.js

--- java.js
+++ java.js
@@ -1,50 +1,43 @@
 const btnEl = document.getElementById("btn");
 const inputEl = document.getElementById("input");
 const cocktailName = document.getElementById("cocktail-name");
-const cocktailImage = document.getElementById("cockatil-image");
+const cocktailImage = document.getElementById("cocktail-image");
 const instructions = document.getElementById("instructions");
 const imageContainer = document.getElementById("container");
-const ingridients = document.getElementById("ingredients");
-
-
-
+const ingredients = document.getElementById("ingredients");
 
 const apiKey = "uZhrMc3NNe79A1cbQY6xkARsHLJlqfvykH3cYY9Y";
-const apiUrl = "https://api.api-ninjas.com/v1/cocktail?name=vodka";
-
+const apiUrl = "https://api.api-ninjas.com/v1/cocktail?name=";
 
 const options = {
-    method: 'GET',
-    headers: { 'X-Api-Key': apiKey,
- },
+  method: 'GET',
+  headers: { 'X-Api-Key': apiKey },
 }
 
+btnEl.addEventListener("click", async () => {
+  const cocktailNameValue = inputEl.value.trim();
+  if (cocktailNameValue === '') {
+    alert('Please enter a cocktail name');
+    return;
+  }
 
- btnEl.addEventListener("click", async () =>{
-    
-    const response = await fetch(apiUrl,options);
-    const data = await response.json()
-    cocktailName.innerHTML = data[0].name
-    instructions.innerHTML= data[0].instructions
-    ingridients.innerHTML = data[0].ingredients
+  try {
+    const response = await fetch(apiUrl + encodeURIComponent(cocktailNameValue), options);
+    const data = await response.json();
 
-
-    
-
-
-
-
-    
-    
-    
-    console.log(data);
-    
-    
-
-    
-
-    
-
-
-
-})
+    if (data.length > 0) {
+      const firstCocktail = data[0];
+      cocktailName.innerHTML = firstCocktail.name;
+      instructions.innerHTML = firstCocktail.instructions;
+      ingredients.innerHTML = "Ingredients: " + firstCocktail.ingredients.join(", ");
+      
+    } else {
+      cocktailName.innerHTML = "Cocktail not found";
+      instructions.innerHTML = "";
+      ingredients.innerHTML = "";
+      cocktailImage.innerHTML = "";
+    }
+  } catch (error) {
+    console.error("Error fetching data:", error);
+  }
+});
